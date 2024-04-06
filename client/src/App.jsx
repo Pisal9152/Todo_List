f8ba54bad4ec7a1b7c49cfb4ec7021daf9eb31e9
Pisal9152 <chhounpisal93@gmail.com>
2024-04-06 22:27:09 +0700

    Your commit message

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TodoList from "./components/TodoList";
+
+const App = () => {
+  return (
+    <div>
+      <TodoList contractAddress={"0x66d520dc75DBf9238672e7E5d264015aA028128d"}/>
+    </div>
+  );
+};
+
+export default App;
